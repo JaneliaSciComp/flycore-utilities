e8abc6a0d76030fddcc6dea9f6713f1ffd4d2e85
Rob Svirskas <robsvi@gmail.com>
2023-03-15 14:12:10 -0400

    Genotype logic added

--- bin/sync_flyf_publishing_names.py
+++ bin/sync_flyf_publishing_names.py
@@ -14,8 +14,7 @@ READ = {'EXISTS': "SELECT id FROM publishing_name WHERE line_id=%s AND publishin
         'LINEID': "SELECT id FROM line WHERE name=%s",
         'SOURCE': "SELECT source_id,id,line FROM publishing_name_vw",
        }
-WRITE = {'DELETE': "DELETE FROM publishing_name WHERE id=%s",
-         'PUBLISHING': "INSERT INTO publishing_name (line_id,source_id,"
+WRITE = {'PUBLISHING': "INSERT INTO publishing_name (line_id,source_id,"
                        + "publishing_name,for_publishing,published,label,display_genotype,"
                        + "requester,notes,source_create_date,preferred_name) "
                        + "VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s) ON "
@@ -28,9 +27,11 @@ CURSOR = {}
 # Configuration
 CONFIG = {'config': {'url': 'https://config.int.janelia.org/'}}
 # General
-COUNT = {'deleted': 0, 'error': 0, 'inserted': 0, 'no_sn': 0, 'not_stock': 0,
-         'read': 0, 'skipped': 0, 'updated': 0, 'publishing_name': 0, 'genotype': 0}
+COUNT = {'deleted': 0, 'format': 0, 'inserted': 0, 'not_stock': 0,
+         'read': 0, 'skipped': 0, 'type': 0, 'flags': 0, 'updated': 0,
+         'publishing_name': 0, 'genotype': 0}
 LINE_ID = {}
+WARNINGS = []
 
 # pylint: disable=W0703,R1710
 
@@ -69,7 +70,7 @@ def call_responder(server, endpoint):
     """
     url = CONFIG[server]['url'] + endpoint
     try:
-        req = requests.get(url, timeout=10)
+        req = requests.get(url, timeout=90)
     except requests.exceptions.RequestException as err:
         LOGGER.critical(err)
         sys.exit(-1)
@@ -129,9 +130,6 @@ def set_publishing_name(line, row):
     if re.search(r"IS\d+", short_line):
         short_line = short_line.replace('IS', 'SS')
     default = 1 if short_line == publishing_name else 0
-    if not row[1]:
-        #LOGGER.error(f"Missing __kp_name_serial_number for _kf_parent_UID {row[0]} ({line})")
-        COUNT['no_sn'] += 1
     utype = 'genotype' if row[6] else 'publishing_name'
     COUNT[utype] += 1
     # Is this an insertion?
@@ -153,40 +151,69 @@ def set_publishing_name(line, row):
         sql_error(err)
 
 
+def error_condition(stockmap, row):
+    if (not row[3]) and (not row[6]):
+        # Skip names without for_publishing or display_genotype
+        COUNT['skipped'] += 1
+        return True
+    elif ARG.LINE and stockmap[row[0]] != ARG.LINE:
+        COUNT['skipped'] += 1
+        return True
+    elif row[3] and row[6]:
+        # Skip names with for_publishing and display_genotype
+        COUNT['flags'] += 1
+        WARNINGS.append(f"{row[0]} {row[2]} has both type flags set")
+        return True
+    elif (not re.search(r"^w[;,-]", row[2])) and row[6]:
+        COUNT['type'] += 1
+        WARNINGS.append(f"{row[0]} {row[2]} may be a publishing name but is flagged as a genotype")
+        return True
+    elif re.search(r"^w[;,-]", row[2]) and row[3]:
+        COUNT['type'] += 1
+        WARNINGS.append(f"{row[0]} {row[2]} may be a genotype but is flagged as a publishing name")
+        return True
+    elif ARG.FILTER and not row[2].startswith(ARG.FILTER):
+        COUNT['skipped'] += 1
+        return True
+    return False
+
+
 def process_single_name(stockmap, row):
     """ Process a single publishing name """
-    if ARG.LINE and stockmap[row[0]] != ARG.LINE:
-        COUNT['skipped'] += 1
-    elif (not ARG.ALL) and (not row[3]) and (not row[6]):
-        # Skip names without for_publishing or display_genome
-        COUNT['skipped'] += 1
-    elif stockmap[row[0]] != row[2]:
-        line = stockmap[row[0]]
-        set_publishing_name(line, row)
-    else:
-        COUNT['skipped'] += 1
+    if not error_condition(stockmap, row):
+        if stockmap[row[0]] != row[2]:
+            line = stockmap[row[0]]
+            set_publishing_name(line, row)
+        else:
+            COUNT['skipped'] += 1
 
 
 def update_publishing_names():
-    """ Get mapping of __kp_UniqueID to stock name """
     stockmap = {}
-    LOGGER.info("Fetching stock names from Fly Core")
-    stocks = call_responder('flycore', '?request=named_stocks')
-    for stock in stocks['stocks']:
-        stockmap[stock] = stocks['stocks'][stock]['Stock_Name']
-    if not stockmap:
-        LOGGER.critical("No stocks found in FLYF2")
-        sys.exit(-1)
-    LOGGER.info("Found %d named stocks in Fly Core", len(stockmap))
+    if not ARG.LINE:
+        """ Get mapping of __kp_UniqueID to stock name """
+        LOGGER.info("Fetching stock names from Fly Core")
+        stocks = call_responder('flycore', '?request=named_stocks')
+        for stock in stocks['stocks']:
+            stockmap[stock] = stocks['stocks'][stock]['Stock_Name']
+        if not stockmap:
+            LOGGER.critical("No stocks found in FLYF2")
+            sys.exit(-1)
+        LOGGER.info("Found %d named stocks in Fly Core", len(stockmap))
     # Get publishing names
     flycore_sn = {}
     LOGGER.info("Fetching publishing names from Fly Core")
-    response = call_responder('flycore', f"?request=publishing_names_sync;days={ARG.DAYS}")
+    if ARG.LINE:
+        response = call_responder('flycore', f"?request=publishing_names_join;line={ARG.LINE}")
+    else:
+        response = call_responder('flycore', f"?request=publishing_names_sync;days={ARG.DAYS}")
     allnames = response['publishing']
     LOGGER.info("Found %d publishing names in Fly Core", len(allnames))
     if not allnames:
         LOGGER.critical("No new names found in FLYF2")
         sys.exit(-1)
+    if ARG.LINE:
+        stockmap[allnames[0][0]] = ARG.LINE
     for row in tqdm(allnames):
         # _kf_parent_UID, __kp_name_serial_number, all_names, for_publishing,
         # published, label, display_genotype, who, notes, create_date
@@ -194,7 +221,7 @@ def update_publishing_names():
         flycore_sn[row[1]] = 1
         if row[0] in stockmap:
             if ("\r" in row[2] or "\n" in row[2]):
-                COUNT['error'] += 1
+                COUNT['format'] += 1
                 LOGGER.error("%s has a publishing name with carriage returns", stockmap[row[0]])
                 continue
             process_single_name(stockmap, row)
@@ -213,33 +240,24 @@ def update_publishing_names():
             sage_source[row[0]] = row[1]
     LOGGER.info("Found %d records in FLYF2", len(flycore_sn))
     LOGGER.info("Found %d records in SAGE", len(sage_source))
-    #if ARG.DELETE:
-    #    for pid in sage_source:
-    #        if pid not in flycore_sn:
-    #            LOGGER.warning("%s is present in SAGE but not in Fly Core", pid)
-    #            LOGGER.debug(WRITE['DELETE'], pid)
-    #            try:
-    #                CURSOR['sage'].execute(WRITE['DELETE'], (pid,))
-    #            except MySQLdb.Error as err:
-    #                sql_error(err)
-    #            COUNT['deleted'] += 1
     if ARG.WRITE:
         CONN['sage'].commit()
+    if WARNINGS:
+        with open("publishing_name_sync.txt", "w", encoding="ascii") as outstream:
+            for line in WARNINGS:
+                outstream.write(f"{line}\n")
 
 
 if __name__ == '__main__':
     PARSER = argparse.ArgumentParser(description="Sync publishing names from Fly Core to SAGE")
     PARSER.add_argument('--manifold', dest='MANIFOLD', action='store',
                         default='prod', help='Database manifold')
-    PARSER.add_argument('--all', dest='ALL', action='store_true',
-                        default=False, help='Process all lines')
     PARSER.add_argument('--days', dest='DAYS', action='store', type=int,
                         default=2, help='Number of days to go back [2]')
+    PARSER.add_argument('--filter', dest='FILTER', action='store',
+                        help='Publishing name filter (starts with)')
     PARSER.add_argument('--line', dest='LINE', action='store',
                         help='Single line to process')
-    PARSER.add_argument('--delete', dest='DELETE', action='store_true',
-                        default=False,
-                        help='Flag, Delete items not in FLYF2 from SAGE')
     PARSER.add_argument('--write', dest='WRITE', action='store_true',
                         default=False,
                         help='Flag, Actually modify database')
@@ -263,14 +281,14 @@ if __name__ == '__main__':
 
     initialize_program()
     update_publishing_names()
-    print(f"Names read:            {COUNT['read']}")
-    print(f"Publishing names:      {COUNT['publishing_name']}")
-    print(f"Genotypes:             {COUNT['genotype']}")
-    print(f"Names inserted:        {COUNT['inserted']}")
-    print(f"Names updated:         {COUNT['updated']}")
-    print(f"Names deleted:         {COUNT['deleted']}")
-    print(f"No stock:              {COUNT['not_stock']}")
-    print(f"Missing serial number: {COUNT['no_sn']}")
-    print(f"Names skipped:         {COUNT['skipped']}")
-    print(f"Names in error:        {COUNT['error']}")
+    print(f"Names read:               {COUNT['read']}")
+    print(f"Publishing names:         {COUNT['publishing_name']}")
+    print(f"Genotypes:                {COUNT['genotype']}")
+    print(f"Names inserted:           {COUNT['inserted']}")
+    print(f"Names updated:            {COUNT['updated']}")
+    print(f"No stock:                 {COUNT['not_stock']}")
+    print(f"Names with bad flags:     {COUNT['flags']}")
+    print(f"Names with type mismatch: {COUNT['type']}")
+    print(f"Names skipped:            {COUNT['skipped']}")
+    print(f"Names with format errors: {COUNT['format']}")
     sys.exit(0)
